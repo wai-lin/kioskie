3a001edb852876f0c910ce0642ffee9749a9b646
wai-lin <dev.wailin@gmail.com>
2025-02-28 17:20:27 +0700

    feat: store model, migration and factory

---
+++ database/factories/StoreFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Store;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class StoreFactory extends Factory
+{
+    protected $model = Store::class;
+
+    public function definition()
+    {
+        return [
+            'name' => fake()->company(),
+        ];
+    }
+}
