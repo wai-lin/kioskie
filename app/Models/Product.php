d3ebc2466acb47706fe12b14d6693522e9a8c3af
wai-lin <dev.wailin@gmail.com>
2025-03-03 19:49:05 +0700

    feat: create App\Models\Product relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the stores that sell the product.
+     */
+    public function stores(): BelongsToMany
+    {
+        return $this->belongsToMany(Store::class)
+            ->withPivot('quantity')
+            ->withTimestamps();
+    }
+
+    /**
+     * Get the transactions that include the product.
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
