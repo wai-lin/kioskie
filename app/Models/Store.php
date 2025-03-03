86b669dc35fb5ec75469430ce2a302dbdf5a75c7
wai-lin <dev.wailin@gmail.com>
2025-03-03 19:46:22 +0700

    feat: create App\Models\Store relations

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use App\Enums\StoreRole;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Store extends Model
 {
@@ -18,5 +20,35 @@ class Store extends Model
         return $this->belongsToMany(User::class)
             ->withPivot('role')
             ->withTimestamps();
+    }
+
+    /**
+     * Store owners.
+     */
+    public function owners(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->withPivot('role')
+            ->wherePivot('role', StoreRole::OWNER->value)
+            ->withTimestamps();
+    }
+
+    /**
+     * Store employees.
+     */
+    public function employees(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->withPivot('role')
+            ->wherePivot('role', StoreRole::EMPLOYEE->value)
+            ->withTimestamps();
+    }
+
+    /**
+     * Store transactions.
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
     }
 }
