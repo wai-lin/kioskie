8e0ee6e84f81874cbedf6faa2f83f8194425e329
wai-lin <dev.wailin@gmail.com>
2025-03-04 16:17:52 +0700

    feat: change stores.card component style

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -8,6 +8,7 @@
     $initial = $user?->name ? strtoupper(mb_substr($user->name, 0, 1)) : '?';
 
     $sizeClasses = [
+        'xs' => 'size-7 text-xs',
         'sm' => 'size-8 text-sm',
         'md' => 'size-10 text-base',
         'lg' => 'size-12 text-lg',
@@ -20,7 +21,7 @@
     >
 @endif
         <div
-            @if($slot->isNotEmpty())
+            @if($user?->name)
                 title="{{$user?->name}}"
             @endif
             class="flex items-center justify-center bg-zinc-200 font-bold rounded-full border-1 border-zinc-300 {{$sizeClasses}}"
--- resources/views/components/stores/card.blade.php
+++ resources/views/components/stores/card.blade.php
@@ -16,13 +16,12 @@
         border-zinc-300
         py-4
         px-8
-        space-y-6
         ring-offset-2
         ring-zinc-500
         hover:ring-1
     "
 >
-    <div class="flex items-end justify-between">
+    <div class="flex items-end justify-between mb-2">
         <h5 class="text-lg font-bold">{{$store->name}}</h5>
 
         <p class="text-sm text-zinc-500">
@@ -30,22 +29,23 @@
         </p>
     </div>
 
-    <div class="flex items-end justify-between">
-        <div class="space-y-2">
-            <h6 class="text-sm text-zinc-500">Owners</h6>
+    <div class="flex items-center gap-1 mb-6">
+        <h6 class="text-sm text-zinc-500">Owners :</h6>
 
-            <div class="flex items-center gap-1">
-                @foreach($first2Owners as $owner)
-                    <x-avatar :user="$owner"/>
-                @endforeach
+        <div class="flex items-center gap-0.5">
+            @foreach($first2Owners as $owner)
+                <x-avatar size="xs" :user="$owner"/>
+            @endforeach
 
-                @if($store->owners->count() > 2)
-                    <x-avatar>
-                        +{{$store->owners->count() - 2}}
-                    </x-avatar>
-                @endif
-            </div>
+            @if($store->owners->count() > 2)
+                <x-avatar size="xs">
+                    +{{$store->owners->count() - 2}}
+                </x-avatar>
+            @endif
         </div>
+    </div>
+
+    <div class="flex items-end justify-between">
 
         @if($href)
             <flux:button size="sm" href="{{$href}}">View Details</flux:button>
