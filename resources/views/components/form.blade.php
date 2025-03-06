03afacc61b5d72518230a8505c09bbec8e3c8bce
wai-lin <dev.wailin@gmail.com>
2025-03-06 16:21:23 +0700

    feat: form component

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,24 @@
+@props([
+    'class',
+    'action',
+    'method' => 'post',
+    'upload' => false,
+])
+
+<form
+    class="{{$class}}"
+    :action="$action"
+    method="POST"
+    @if($upload) enctype="multipart/form-data" @endif
+>
+    @csrf
+    @if($method == 'put')
+        @method('PUT')
+    @elseif($method == 'patch')
+        @method('PATCH')
+    @elseif($method == 'delete')
+        @method('DELETE')
+    @endif
+
+    {{$slot}}
+</form>
