d2aaf9e58146e20b7aec3e8c1d0bb4011f680579
wai-lin <dev.wailin@gmail.com>
2025-03-07 14:09:43 +0700

    feat: products.index view

---
+++ resources/views/livewire/products/index.blade.php
@@ -0,0 +1,24 @@
+<x-layouts.app>
+    <div class="mb-4 flex items-center justify-between">
+        <flux:breadcrumbs>
+            <flux:breadcrumbs.item>Products</flux:breadcrumbs.item>
+        </flux:breadcrumbs>
+
+        <flux:button size="sm" variant="primary" icon="squares-plus" :href="route('stores.create')">
+            Create Product
+        </flux:button>
+    </div>
+
+    <div class="py-4">
+        {{$products->links()}}
+    </div>
+
+    <section class="grid grid-cols-4 gap-8">
+        @foreach($products as $product)
+            <x-products.card
+                :product="$product"
+                :href="route('products.show', $product)"
+            />
+        @endforeach
+    </section>
+</x-layouts.app>
