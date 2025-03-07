bdc211daa9f124dea6ae0c12112dc1896f2c4155
wai-lin <dev.wailin@gmail.com>
2025-03-07 14:07:48 +0700

    feat: change application logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,7 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+<div class="flex aspect-square size-10 items-center justify-center rounded-md bg-accent">
+{{--    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />--}}
+    <img alt="" src="{{ Vite::asset('resources/images/kiosk.png') }}" class="h-5/6" />
 </div>
 <div class="ml-1 grid flex-1 text-left text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+    <span class="mb-0.5 truncate leading-none font-semibold">Kioskie</span>
 </div>
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,10 +1,11 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+<link rel="icon" href="{{Vite::asset('resources/images/kiosk.png')}}" >
 <title>{{ $title ?? 'Laravel' }}</title>
 
 <link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" >
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
