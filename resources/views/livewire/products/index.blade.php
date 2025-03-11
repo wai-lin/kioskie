b4ab1a2dc491bc89bd83ff903e38d39a68660207
wai-lin <dev.wailin@gmail.com>
2025-03-11 15:39:35 +0700

    refactor: migrate to products.list component

--- resources/views/livewire/products/index.blade.php
+++ resources/views/livewire/products/index.blade.php
@@ -13,12 +13,5 @@
         {{$products->links()}}
     </div>
 
-    <section class="grid grid-cols-4 gap-8">
-        @foreach($products as $product)
-            <x-products.card
-                :product="$product"
-                :href="route('products.show', $product)"
-            />
-        @endforeach
-    </section>
+    <x-products.list :products="$products" />
 </x-layouts.app>
--- resources/views/livewire/stores/show.blade.php
+++ resources/views/livewire/stores/show.blade.php
@@ -3,4 +3,6 @@
         <flux:breadcrumbs.item :href="route('stores.index')">Stores</flux:breadcrumbs.item>
         <flux:breadcrumbs.item>{{$store->name}}</flux:breadcrumbs.item>
     </flux:breadcrumbs>
+
+    <x-products.list :products="$store->products" />
 </x-layouts.app>
