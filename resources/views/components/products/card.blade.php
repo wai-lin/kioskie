f143763982255fbabfd026c5a1df14dfc0fb353e
wai-lin <dev.wailin@gmail.com>
2025-03-12 00:39:33 +0700

    feat: update product card with image

--- resources/views/components/products/card.blade.php
+++ resources/views/components/products/card.blade.php
@@ -12,70 +12,76 @@
 @endphp
 
 <x-card>
-    <div class="flex flex-col justify-between mb-4">
-        <h5 title="{{$product->name}}" class="text-lg font-bold truncate max-w-[90%]">
-            {{$product->name}}
-        </h5>
+    <div class="flex items-center gap-4">
+        <x-image :src="$product->getFirstMediaUrl('products.'.$product->id)" class="size-28 rounded-lg flex-none" />
 
-        <p class="text-sm text-zinc-500 flex gap-1">
-            <span>{{$product->price}}</span>
-            <span>THB</span>
-        </p>
+        <div class="flex-auto">
+            <div class="flex flex-col justify-between mb-4">
+                <h5 title="{{$product->name}}" class="text-lg font-bold truncate max-w-[90%]">
+                    {{$product->name}}
+                </h5>
 
-        @if($quantity > 0)
-            <p class="flex items-end gap-1 text-sm {{$quantityColor}}">
-                <span>Qty :</span>
-                <span>{{$quantity}}</span>
-            </p>
-        @endif
-    </div>
+                <p class="text-sm text-zinc-500 flex gap-1">
+                    <span>{{$product->price}}</span>
+                    <span>THB</span>
+                </p>
 
-    <div class="flex items-end justify-between">
-        <div>
-            @if($href)
-                <flux:button size="sm" variant="filled" class="text-xs" :href="$href">
-                    View Details
-                </flux:button>
-            @endif
-        </div>
-
-        @auth
-            <div>
-                <x-form
-                    method="delete"
-                    id="products.delete.{{$product->id}}"
-                    :action="route('products.destroy', $product)"
-                />
-
-                <flux:dropdown>
-                    <flux:button size="sm" icon="ellipsis-horizontal"/>
-
-                    <flux:menu>
-                        @if($canEditStock)
-                            <flux:menu.item
-                                icon="inbox-stack"
-                                :href="route('stores.edit_stock', [$store, $product])"
-                            >
-                                Edit Stock
-                            </flux:menu.item>
-                        @endif
-                        <flux:menu.item
-                            icon="pencil"
-                            :href="route('products.edit', $product)"
-                        >
-                            Edit
-                        </flux:menu.item>
-                        <flux:menu.item
-                            icon="trash"
-                            variant="danger"
-                            type="submit"
-                            form="products.delete.{{$product->id}}"
-                        >
-                            Delete
-                        </flux:menu.item>
-                    </flux:menu>
-                </flux:dropdown>
+                @if($quantity > 0)
+                    <p class="flex items-end gap-1 text-sm {{$quantityColor}}">
+                        <span>Qty :</span>
+                        <span>{{$quantity}}</span>
+                    </p>
+                @endif
             </div>
-        @endauth
+
+            <div class="flex items-end justify-between">
+                <div>
+                    @if($href)
+                        <flux:button size="sm" variant="filled" class="text-xs" :href="$href">
+                            View Details
+                        </flux:button>
+                    @endif
+                </div>
+
+                @auth
+                    <div>
+                        <x-form
+                            method="delete"
+                            id="products.delete.{{$product->id}}"
+                            :action="route('products.destroy', $product)"
+                        />
+
+                        <flux:dropdown>
+                            <flux:button size="sm" icon="ellipsis-horizontal"/>
+
+                            <flux:menu>
+                                @if($canEditStock)
+                                    <flux:menu.item
+                                        icon="inbox-stack"
+                                        :href="route('stores.edit_stock', [$store, $product])"
+                                    >
+                                        Edit Stock
+                                    </flux:menu.item>
+                                @endif
+                                <flux:menu.item
+                                    icon="pencil"
+                                    :href="route('products.edit', $product)"
+                                >
+                                    Edit
+                                </flux:menu.item>
+                                <flux:menu.item
+                                    icon="trash"
+                                    variant="danger"
+                                    type="submit"
+                                    form="products.delete.{{$product->id}}"
+                                >
+                                    Delete
+                                </flux:menu.item>
+                            </flux:menu>
+                        </flux:dropdown>
+                    </div>
+                @endauth
+            </div>
+        </div>
     </div>
 </x-card>
