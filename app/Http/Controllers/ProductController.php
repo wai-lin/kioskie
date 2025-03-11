207e48ebc4070a5404db83d89c1b8519bcb24a8a
wai-lin <dev.wailin@gmail.com>
2025-03-11 16:05:19 +0700

    feat: ProductController edit and destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ProductController extends Controller
 {
@@ -35,9 +36,30 @@ class ProductController extends Controller
         return redirect()->route('products.index');
     }
 
-    public function edit($id) {}
+    public function edit(Product $product) {
+        return view('livewire.products.edit', compact('product'));
+    }
 
-    public function update(Request $request, $id) {}
+    public function update(Request $request, Product $product) {
+        $request->validate([
+            'code' => [
+                'required', 'alpha_num',
+                Rule::unique('products')->ignore($product->id)
+            ],
+            'name' => 'required|string|min:1|max:255',
+            'price' => 'required|numeric|min:0',
+        ]);
 
-    public function destroy($id) {}
+        $product->update($request->all());
+
+        return redirect()
+            ->route('products.index')
+            ->with('info', 'Product updated successfully');
+    }
+
+    public function destroy(Product $product) {
+        $product->delete();
+
+        return redirect()->route('products.index');
+    }
 }
